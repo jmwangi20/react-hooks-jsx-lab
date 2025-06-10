60d9b205130e5de0dd9d38db3e06bc4b739acdc7
John Kihugo Mwangi <johnmwangi13119@gmail.com>
2025-06-10 17:08:31 +0300

    initial commit

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../src/data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id = "about">
+      <h2>About me</h2>
+      <p>This is some information about my background as a web developer</p>
+      <img src={user.image} alt="I made this" />
+    </div>
+  )
 }
 
 export default About;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
-function Home() {
+function Home({name , city}) {
   // update the JSX being returned!
-  return <div>Home</div>;
-}
-
+  return (
+    <div id = "home">
+      <h1 style = {{color : "firebrick"}}>
+        {name} is a web developer from {city}
+      </h1>
+    </div>
+  )}
 export default Home;
